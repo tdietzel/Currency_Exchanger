c2cd58f6132da20ed93925ecb17d12267e4d51a7
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-11-17 08:22:14 -0800

    Update name & title

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Shape Tracker",
+      title: "Currency Exchange",
       template: "./src/index.html",
       inject: "body",
     }),
