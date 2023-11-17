a469510ce02eeb2f48655b5ee08558292a4d6291
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-11-17 09:20:48 -0800

    Add Exchange class & getCurrencyRates to fetch exchange rate API

---
+++ src/getExchange.js
@@ -0,0 +1,13 @@
+export default class Exchange {
+  static async getCurrencyRates() {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return await response.json();
+    } catch (error) {
+      return error.message;
+    }
+  }
+}
