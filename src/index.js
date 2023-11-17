79fcf285ef54a148e68361a6783e319e8ca23317
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-11-17 15:27:13 -0800

    Add calcExchange.js with all 161 countries conversions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Exchange from './getExchange';
+import calculateExchange from './calcExchange';
 
 // UI
 export default function printError(error) {
@@ -10,19 +11,6 @@ export default function printError(error) {
 
 function printResults(result) {
   document.querySelector("p#results").innerText = result;
-}
-
-// Business Logic
-
-function calculateExchange(selectedExchangeTo, response) {
-  const currencyAmount = document.querySelector("input#userCurrencyAmount").value;
-  switch (selectedExchangeTo) {
-  case 'AED': return currencyAmount * response.conversion_rates.AED;
-  case 'AFN': return currencyAmount * response.conversion_rates.AFN;
-  case 'ALL': return currencyAmount * response.conversion_rates.ALL;
-  case 'AMD': return currencyAmount * response.conversion_rates.AMD;
-  case 'ANG': return currencyAmount * response.conversion_rates.ANG; 
-  }
 }
 
 function getExchangeRate(response) {
