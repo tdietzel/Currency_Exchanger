b5c74941c2fac4b4c49193f5316cc24a8e18977b
Trent Dietzel <147360049+tdietzel@users.noreply.github.com>
2023-11-19 22:26:03 -0800

    Add API link

--- src/getExchange.js
+++ src/getExchange.js
@@ -3,6 +3,7 @@ import printError from "./index";
 export default class Exchange {
   static async getCurrencyRates(exchange) {
     try {
+      // https://www.exchangerate-api.com/docs/overview
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${exchange}`);
       if (!response.ok) {
         throw Error(`Failed to fetch currency rates. Status: ${response.status}`);
